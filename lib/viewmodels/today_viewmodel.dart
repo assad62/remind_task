8c38bea2183a3584ae3bc3414f63c838302d2eb2
Home User 2 <assad62@hotmail.com>
2021-12-13 20:44:37 +0800

    add push to add task view

--- lib/viewmodels/today_viewmodel.dart
+++ lib/viewmodels/today_viewmodel.dart
@@ -1,8 +1,16 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_starter_app/common/base_model.dart';
+import 'package:flutter_starter_app/common/navigation_service.dart';
+import 'package:flutter_starter_app/ui/router.dart';
+
+import '../locator.dart';
 
 class TodayViewModel extends BaseModel{
+  var _context = locator<NavigationService>().navigatorKey.currentContext!;
+
   void addNewTask(){
 
+    Navigator.pushNamed(_context, AppRoutes.createTaskPageRoute);
   }
 
   void onFirstLoad(){
